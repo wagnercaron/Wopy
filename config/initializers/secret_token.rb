13996414c6abdfda111c92ac2d1a3487c5896e66
Wagner <wagner.caron@gmail.com>
2011-11-06 17:06:56 -0200

    the first ruby example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psp::Application.config.secret_token = '11151c95e672c18d55b34da79a9f431274b668dfa642e660af266b6618a9e41961116274b9724db354d1b5c7f51b5ad112374ddbb7e3918f2d47a22a2c386114'
